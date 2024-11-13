92f75e93af324f1de5eb61b748e7faf19f3e2893
bryanbayup <bryanazza836@gmail.com>
2024-11-13 16:44:54 +0700

    update app.py

--- app.py
+++ app.py
@@ -108,7 +108,8 @@ model_intent, model_ner = load_models()
 
 # Mendefinisikan max_len berdasarkan model
 max_len = model_intent.input_shape[1]
-max_len_ner = model_ner.input_shape[1]
+# Karena model_ner adalah ModelWithCRFLoss, yang tidak memiliki input_shape, tetapkan max_len_ner secara manual
+max_len_ner = max_len  # Pastikan ini sesuai dengan yang digunakan saat pelatihan
 
 # Fungsi Mengubah Indeks ke Tag
 def sequences_to_tags(sequences, idx2tag):
@@ -171,17 +172,12 @@ def get_chatbot_response(user_input):
             entities_dict[label].append(token)
         else:
             entities_dict[label] = [token]
-
-    # Mengambil jenis hewan dan gejala
-    animal = ', '.join(entities_dict.get('animal', ['tidak disebutkan']))
-    symptoms = ', '.join(entities_dict.get('symptom', ['tidak disebutkan']))
-
-    # Menentukan respon berdasarkan intent
+    # Find the appropriate response
     intent_data_item = next((item for item in intent_data['intents'] if item['intent'] == intent), None)
     if intent_data_item:
         response_template = random.choice(intent_data_item['responses'])
-        # Mengisi placeholder dalam respon
-        for entity_label in ['animal', 'symptom', 'condition', 'treatment']:
+        # Replace placeholders in the response
+        for entity_label in ['animal', 'condition', 'symptom', 'treatment']:
             placeholder = '{' + entity_label + '}'
             if placeholder in response_template:
                 value = ', '.join(entities_dict.get(entity_label, ['tidak disebutkan']))
